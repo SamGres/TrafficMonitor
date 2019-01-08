0fc6f8dfc78a75006ca78486f8c5894bcdfff839
KoprivcLuka <45632449+KoprivcLuka@users.noreply.github.com>
2019-01-08 14:33:24 +0100

    Maske

--- Android/app/src/main/java/com/example/lukak/samgre/MainActivity.java
+++ Android/app/src/main/java/com/example/lukak/samgre/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.lukak.samgre;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+                Button Login = (Button) this.findViewById(R.id.button);
+                Login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        startActivity(new Intent(view.getContext(),Map.class));
+
+                    }
+                });
+        final Button register = (Button) this.findViewById(R.id.button2);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(view.getContext(),Register.class));
+
+            }
+        });
 
     }
 }
