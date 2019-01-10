988316301b90966334047030ee6f9bd7b30c18a4
KoprivcLuka <45632449+KoprivcLuka@users.noreply.github.com>
2019-01-10 23:02:34 +0100

    MapaDela

--- Android/app/src/main/java/com/example/lukak/samgre/Map.java
+++ Android/app/src/main/java/com/example/lukak/samgre/Map.java
@@ -13,7 +13,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class Map extends Fragment{
+public class Map extends Fragment implements OnMapReadyCallback {
 
     private static final String MAP_VIEW_BUNDLE_KEY = "AIzaSyC-GjITQrAeucuUZj6104a5EL0uQIi1WwU";
     private MapView mapView;
@@ -36,23 +36,69 @@ public class Map extends Fragment{
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
         }
 
-        MapView mapView = view.findViewById(R.id.mapView);
+        mapView = view.findViewById(R.id.mapView);
         mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(new OnMapReadyCallback() {
+        mapView.getMapAsync(this);
 
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                gmap = googleMap;
-                LatLng sydney = new LatLng(-34, 151);
-                gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-                gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
-            }
-        });
+
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        gmap = googleMap;
+        LatLng sydney = new LatLng(-34, 151);
+        gmap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        gmap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
 
 
 
-  
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Bundle mapViewBundle = outState.getBundle(MAP_VIEW_BUNDLE_KEY);
+        if (mapViewBundle == null) {
+            mapViewBundle = new Bundle();
+            outState.putBundle(MAP_VIEW_BUNDLE_KEY, mapViewBundle);
+        }
+
+        mapView.onSaveInstanceState(mapViewBundle);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    public void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    public void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+
+}
+
 }
--- Android/app/src/main/java/com/example/lukak/samgre/Register.java
+++ Android/app/src/main/java/com/example/lukak/samgre/Register.java
@@ -1,7 +1,10 @@
 package com.example.lukak.samgre;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Register extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        final Button register = (Button) this.findViewById(R.id.button3);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               finish();
+
+            }
+        });
     }
 }
