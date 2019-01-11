9cb1ebbe6daea156b1d4a63a68044ffeca084212
Zazo <45300078+SamGres@users.noreply.github.com>
2019-01-11 15:55:15 +0100

    Update TokenGenerator.java

--- api/project/src/main/java/si/samgres/api/helpers/TokenGenerator.java
+++ api/project/src/main/java/si/samgres/api/helpers/TokenGenerator.java
@@ -1,13 +1,9 @@
 package si.samgres.api.helpers;
 
-import java.security.SecureRandom;
 import java.util.UUID;
 
 public class TokenGenerator {
     public static String generate() {
-        SecureRandom random = new SecureRandom();
-        byte bytes[] = new byte[128];
-        random.nextBytes(bytes);
-        return bytes.toString();
+        return UUID.randomUUID().toString();
     }
 }
