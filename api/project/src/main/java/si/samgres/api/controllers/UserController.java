8fc8d84adae9083ca993817b123808987e642e59
Zazo <45300078+SamGres@users.noreply.github.com>
2019-01-18 13:13:35 +0100

    Update UserController.java

--- api/project/src/main/java/si/samgres/api/controllers/UserController.java
+++ api/project/src/main/java/si/samgres/api/controllers/UserController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import si.samgres.api.services.TrafficService;
 import si.samgres.api.services.UserService;
 
 @RestController
