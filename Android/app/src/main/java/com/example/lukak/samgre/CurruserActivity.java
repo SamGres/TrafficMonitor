19b7dd217a7947eace2c93439cf7030eaf1963fb
KoprivcLuka <45632449+KoprivcLuka@users.noreply.github.com>
2019-01-18 12:50:20 +0100

    Sync

--- Android/app/src/main/java/com/example/lukak/samgre/CurruserActivity.java
+++ Android/app/src/main/java/com/example/lukak/samgre/CurruserActivity.java
@@ -35,7 +35,7 @@ public class CurruserActivity extends Activity {
         btnObjave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //Change fragment dude
+                startActivity(new Intent(getApplicationContext(),ObvestilaActivity.class));
             }
         });
     }
